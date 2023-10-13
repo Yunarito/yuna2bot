24ebd4204251530e8d0df43b3764134d1eac8d41
Yunarito <48757001+Yunarito@users.noreply.github.com>
2023-10-13 19:13:50 +0200

    removeing stuff

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-export const CHANNEL_NAME = '#yunarito,#rosnitzky,#chris5560,#janniadhara';
-export const OAUTH_TOKEN = 'oauth:53wrxjeullhhe5of7vi306ms1gy0x5';
-export const BOT_USERNAME = 'yuna2bot';
-export const RIOT_API_TOKEN = 'RGAPI-765a148c-1977-4440-ac65-ee3da742d148';
+export const CHANNEL_NAME = '';
+export const OAUTH_TOKEN = 'oauth:';
+export const BOT_USERNAME = '';
+export const RIOT_API_TOKEN = '';
 export const BLOCKED_WORDS = [
 ]
