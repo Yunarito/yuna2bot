3cb23328cc057952615b6106d1a27d98b4cdcb0d
Yunarito <48757001+Yunarito@users.noreply.github.com>
2023-10-28 14:12:58 +0200

    added timer

--- src/app.js
+++ src/app.js
@@ -67,7 +67,7 @@ client.on('message', (channel, userstate, message, self) => {
   {
     if(channel.includes('catzzi')){
       let names = ['catzzi', 'I Love U much']
-      names.forEach(name => setTimeout(() => getSummonerRank(channel, userstate, '!rank ' + name), 500))
+      names.forEach(name => setTimeout(() => getSummonerRank(channel, userstate, '!rank ' + name), 1000))
     } else {
       getSummonerRank(channel, userstate, message)
     }
