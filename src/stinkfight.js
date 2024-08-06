b30fa32cc3052eb12ba3e4d8fc2ca4ca6bd693e3
Yunarito <48757001+Yunarito@users.noreply.github.com>
2024-08-06 15:37:50 +0200

    added explanation

--- src/stinkfight.js
+++ src/stinkfight.js
@@ -25,7 +25,8 @@ export function duel(channel, userstate, message) {
     }
     
     channelData.pendingDuels[username] = { opponent, timeout: null };
-    client.say(channel, `@${opponent}, you have been challenged to a duel by @${username}! Type !accept to accept the challenge.`);
+    client.say(channel, `@${opponent}, you have been challenged to a duel by @${username}! 
+      Type !accept to accept the challenge. Loser will be 5 mintes in Timeout .`);
   
     channelData.pendingDuels[username].timeout = setTimeout(() => {
       client.say(channel, `@${username}, your duel request to @${opponent} has expired.`);
