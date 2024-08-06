6379405abf71b894627412814f9bc339ab28f59f
Yunarito <48757001+Yunarito@users.noreply.github.com>
2024-08-06 15:17:21 +0200

    fixings

--- src/userStats.js
+++ src/userStats.js
@@ -68,7 +68,7 @@ function updateUserStats(channel, username, isWinner) {
 // Function to handle the stats command
 function stats(channel, userstate, message) {
     let command = message.trim().split(' ');
-    const username = command.length < 2 ? userstate.username : command[1].replace('@', '').tolowerCase();
+    const username = command.length < 2 ? userstate.username : command[1].tolowerCase().replace('@', '');
     const stats = readUserStats();
     const userStats = (stats[channel] && stats[channel][username]) || { wins: 0, losses: 0 };
   
