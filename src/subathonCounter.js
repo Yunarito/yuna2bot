9b3ef30d9fff4c13e478d8430cfb71da1d0922dc
Yunarito <48757001+Yunarito@users.noreply.github.com>
2024-09-11 20:55:26 +0200

    changes

--- src/subathonCounter.js
+++ src/subathonCounter.js
@@ -119,7 +119,7 @@ export function getChannelPoints(channel, username) {
     }
 
 
-    let points = subathonData[channel][username].points;
+    let points = Math.round(subathonData[channel][username].points, 2)+"".replace('.', ',');
     client.say(channel, `@${username}, du hast ${points} Punkte zum Subathon beigetragen.`);
 }
 
@@ -133,6 +133,6 @@ export function getChannelTotalPoints(channel) {
         writeSubathonData(subathonData);
     }
 
-    let points = subathonData[channel].points;
+    let points = Math.round(subathonData[channel].points, 2)+"".replace('.', ',');
     client.say(channel, `Der aktuelle Subathon hat ${points} Punkte.`);
 }
