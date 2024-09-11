1f0ef923acffced4d4d7bbeff0934b06f4ea14f1
Yunarito <48757001+Yunarito@users.noreply.github.com>
2024-09-11 20:59:21 +0200

    yes

--- src/subathonCounter.js
+++ src/subathonCounter.js
@@ -119,7 +119,7 @@ export function getChannelPoints(channel, username) {
     }
 
 
-    let points = Math.round(subathonData[channel][username].points * 100)/100+"".replace('.', ',');
+    let points = Math.round(subathonData[channel][username].points * 100)/100+"".replace(',', '.');
     client.say(channel, `@${username}, du hast ${points} Punkte zum Subathon beigetragen.`);
 }
 
