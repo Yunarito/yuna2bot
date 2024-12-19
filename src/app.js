4742b0a7a0c84cf9fd03c6a629bef47a53fedfe2
Yunarito <48757001+Yunarito@users.noreply.github.com>
2024-12-19 12:50:16 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -113,7 +113,7 @@ client.on('message', (channel, userstate, message, self) => {
   if (startsWith(message, '!rank') || startsWith(message, '!elo')) {
     if(channel === '#catzzi' && message.includes('mods')) {
       getSummonerRank(channel, userstate, "!rank Yunarito#69420,Leaveless#GGA,scremmys#6969");
-    } else if(channel === '#catzzi') {
+    } else if(channel === '#catzzi' && !message.includes('#')) {
       getSummonerRank(channel, userstate, "!rank catzzi#euw,smolercatzzi#6969,smolestcatzzi#6969"); //catzzi#euw,smolcatzzi#EUW,smolercatzzi#6969,smolestcatzzi#6969
     } else {
       getSummonerRank(channel, userstate, message);
